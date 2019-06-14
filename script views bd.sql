07866f028db52c49cdd116c5d066bb7f78327b5e
Flávio Sousa <fn_maia@outlook.com>
2019-06-13 21:09:59 -0300

    Adicionado script de views

---
+++ script views bd.sql
@@ -0,0 +1,9 @@
+/*
+ * Flávio Sousa - 115037071
+ * João Maia - 116111909
+ * João Pedro -
+ * Banco de Dados
+ * Trabalho prático 1 - Script de criação dos papéis e views do banco de dados
+ */
+
+USE aluguelcarros;
