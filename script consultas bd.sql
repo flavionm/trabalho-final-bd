f280d3af1fd3eccc62eb966af9c4ded7c8ef333f
JoaoRMaia <JoaoRMaia@users.noreply.github.com>
2019-06-17 22:18:10 -0300

    BugFix Consultas

--- script consultas bd.sql
+++ script consultas bd.sql
@@ -9,7 +9,7 @@
 # 1-Listar o ID de todos os carros indisponíveis  (UNION)
 
 SELECT IDCARRO 
-FROM aluguel 
+FROM aluguel
 WHERE retorno > CURDATE() OR retorno IS NULL
 UNION
 SELECT IDCARRO 
@@ -22,17 +22,17 @@ SELECT idcarro, modelo
 FROM carro 
 WHERE fabricante IN ('AUDI','VOLVO','FIAT','VOLKSWAGEN');
 
-# 3-Listar todos os modelos com menos de 3 carros disponíveis.  (GROUP BY,HAVING)
+# 3-Listar todos os modelos com menos de 3 carros.  (GROUP BY,HAVING)
 
 SELECT COUNT(*) QUANTIDADE, MODELO FROM CARRO GROUP BY MODELO HAVING COUNT(*) < 3;
 
-# 4-Listar todos os clientes que alugaram todos os modelos de carro  (DIVISÃO,+3T)  ERRO
+# 4-Listar todos os clientes que alugaram todos os modelos de carro  (DIVISÃO,+3T)
 
 SELECT nome, COUNT(DISTINCT MODELO) AS '#MODELODIFERENTE'
 FROM ALUGUEL 
 INNER JOIN CARRO ON CARRO.IDCARRO = ALUGUEL.IDCARRO
 INNER JOIN cliente ON cliente.idcliente = aluguel.idcliente
-INNER JOIN pessoa ON cliente.cpf = pessoa.cpf
+INNER JOIN pessoa ON cliente.cpf = pessoa.cpf GROUP BY nome
 HAVING COUNT(DISTINCT MODELO) = (SELECT  COUNT(DISTINCT MODELO) FROM CARRO);
 
 # 5-Listar todos os clientes que alugaram um UNO  (EXISTS,+3T)
@@ -47,7 +47,7 @@ WHERE EXISTS
     WHERE aluguel.idcliente = cliente.idcliente AND carro.MODELO = 'Uno');
     
 # 6-Mostrar o quanto cada cliente gastou no total (+3T)
-SELECT nome,  'R$'+FORMAT(DATEDIFF(retorno, retirada) * preco,2) AS 'total gasto'
+SELECT nome,  CONCAT('R$ ',FORMAT(DATEDIFF(retorno, retirada) * preco,2)) AS 'total gasto'
 FROM pessoa 
 INNER JOIN cliente ON cliente.cpf = pessoa.cpf
 INNER JOIN aluguel ON aluguel.idcliente = cliente.idcliente
@@ -88,17 +88,17 @@ INNER JOIN redehoteis ON parceiro.idparceiro = redehoteis.idparceiro
 INNER JOIN hotel ON redehoteis.idredehoteis = hotel.idredehoteis
 WHERE hotel.localizacao = "Rio de Janeiro";
 
-# 11 - Buscar os funcionários que são gerenciados por alguém com Lucas no nome (+3T)
+# 11 - Buscar os funcionários que são gerenciados por alguém com Marcos no nome (+3T)
 
 SELECT gerentePessoa.nome,gerenciadoPessoa.nome
 FROM agente agentegerente 
 INNER JOIN funcionario gerentes ON gerentes.idfuncionario = agentegerente.idfuncionario
-inner join pessoa gerentePessoa on gerentePessoa.cpf = gerentes.cpf
+INNER JOIN pessoa gerentePessoa ON gerentePessoa.cpf = gerentes.cpf
 INNER JOIN gerencia ON gerencia.idgerente = agentegerente.idagente
 INNER JOIN agente agentegerenciado ON agentegerenciado.idagente = gerencia.idagente
 INNER JOIN funcionario gerenciado ON gerenciado.idfuncionario = agentegerenciado.idfuncionario
-inner join pessoa gerenciadoPessoa on gerenciadoPessoa.cpf = gerenciado.cpf
-WHERE gerentePessoa.nome LIKE "%Lucas%";
+INNER JOIN pessoa gerenciadoPessoa ON gerenciadoPessoa.cpf = gerenciado.cpf
+WHERE gerentePessoa.nome LIKE "%Marcos%";
 
 # 12 - Buscar o nome de todos os caminhoneiros que não possuem caminhão  (+3T)
 
@@ -113,6 +113,6 @@ WHERE caminhao.idcaminhao IS NULL;
 
 SELECT DISTINCT carro.*
 FROM carro 
-LEFT JOIN aluguel ON aluguel.idcarro = carro.idcarro
-LEFT JOIN manutencao ON manutencao.idcarro = aluguel.idcarro
-WHERE SAIDA <= CURDATE() OR RETORNO <= CURDATE() OR (saida IS NULL AND retorno IS NULL)
+WHERE idcarro NOT IN(SELECT idcarro FROM aluguel WHERE RETORNO > CURDATE())
+AND idcarro NOT IN(SELECT idcarro FROM manutencao WHERE SAIDA > CURDATE());
+
